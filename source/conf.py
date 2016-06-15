8f5d500887f2f11d37af3ec04f5b9ce4cabe719c
Takanori Suzuki <takanory.net@gmail.com>
2016-06-15 15:04:53 +0900

    ISSHA-290 フッターに CC-BY とライセンスを明記

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyCon JP 運営マニュアル'
-copyright = u'2016, takanory'
+copyright = u''
 author = u'takanory'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -179,7 +179,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
